6bbfaf07c4412743e2fc9ff82f842b907e40f3ea
taken1994 <601367440@qq.com>
2016-08-07 11:55:15 +0800

    1

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+import marked from 'marked';
 class Post extends React.Component {
   constructor(){
     super();
@@ -10,19 +10,20 @@ class Post extends React.Component {
   }
   componentDidMount(){
     // use math random to avoid browser cache
-    let address=`https://raw.githubusercontent.com/taken1994/big-demo/master/posts/css.md?v=${Math.random()}`;
+    let address=`https://raw.githubusercontent.com/taken1994/big-demo/master/posts/${this.props.params.title}.md?v=${Math.random()}`;
     axios.get(address).then((res) => {
         console.log(res);
         this.setState({
-          Content: res.data,
+          Content: res.data
         });
       });
     }
+
   render () {
+    let content=marked(this.state.Content !='' ? this.state.Content : 'loading')
     return(
       <div>
-        {this.props.params.title}
-        {this.state.Content}
+        <div dangerouslySetInnerHTML={{__html: content}} />
       </div>
     )
   }
